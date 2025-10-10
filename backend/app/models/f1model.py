910e62c814ca6b00c1985dba5f04b0b5b865e629
Hafizmuhammedk <hafizmuhammed1019@gmail.com>
2025-10-10 06:30:17 +0000

    page

--- backend/app/models/f1model.py
+++ backend/app/models/f1model.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, Date
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship 
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -18,11 +18,51 @@ class Team(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
 
+    car_team = relationship("car_team", back_populates="Team")
+    team = relationship("racers", back_populates="Team")
 
-class car(Base):
+
+class Car(Base):
     __tablename__ = "car"
     id = Column(Integer, primary_key=True, index=True)
     model = Column(String, nullable=False)
 
+    car_team = relationship("car_team", back_populates="Car")
 
-class 
+
+class Winner(Base):
+    __tablename__ = "winner"
+    year = Column(Integer, nullable=False)
+    id = Column(Integer, primary_key=True, index=False)
+
+    winner_year = relationship("Winner_year", back_populates="winner")
+
+
+class Racers(Base):
+    __tablename__ = "racers"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    team = Column(Integer, ForeignKey("team.id"))
+
+    winner_year = relationship("winner_year", back_populates="Racers")
+    team = relationship("Team", back_populates="racers")
+
+
+class Car_team(Base):
+    __tablename__ = "car_team"
+    team_id = Column(Integer, ForeignKey("team.id"))
+    car_id = Column(Integer, ForeignKey("car.id"))
+
+    car = relationship("Car", back_populates="car_team")
+    team = relationship("Team", back_populates="car_taem")
+
+
+class Winner_year(Base):
+    __tablename__ = "winner_year"
+    winner_id = Column(Integer, ForeignKey("winner.id"))
+    position = Column(Integer, nullable=False)
+    racer_id = Column(Integer, ForeignKey("racers.id"))
+    point = Column(Integer, nullable=False)
+
+    winner = relationship("Winner", back_populates="winner_year")
+    racer = relationship("Racers", back_populates="winner_year")
